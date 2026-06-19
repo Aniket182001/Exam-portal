1468741e7787d9859215fafbfde4b8707c966aa7
Aniket182001 <aniketkumbhar.ak36@gmail.com>
2026-06-19 16:52:41 +0530

    Auto-open browser on app start

--- run.py
+++ run.py
@@ -1,6 +1,14 @@
+import os
+import webbrowser
+from threading import Timer
 from app import create_app
 
 app = create_app()
 
+def open_browser():
+    webbrowser.open("http://127.0.0.1:5000")
+
 if __name__ == "__main__":
+    if os.environ.get("WERKZEUG_RUN_MAIN") != "true":
+        Timer(1, open_browser).start()
     app.run(debug=True)
